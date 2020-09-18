37b7041d0f22d5de6de9b6273b07c504e2fe005a
Javier Camelis <jcamelis@gmail.com>
2020-09-18 20:09:50 -0300

    section 4 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { CurrentTrainingComponent } from './training/current-training/current-tr
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingsComponent } from './training/past-trainings/past-trainings.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { StopDialogComponent } from './training/current-training/stop-dialog/stop-dialog.component';
+
 
 @NgModule({
   declarations: [
@@ -24,7 +28,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingsComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    StopDialogComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +43,9 @@ import { WelcomeComponent } from './welcome/welcome.component';
     ReactiveFormsModule
   ],
   providers: [],
+  entryComponents: [
+    StopDialogComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { StopDialogComponent } from './stop-dialog/stop-dialog.component';
 
 @Component({
   selector: 'app-current-training',
@@ -7,9 +9,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentTrainingComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+  public trainingExit = new EventEmitter();
+
+  progress = 0;
+
+  timer;
+
+  paused: boolean;
+
+  constructor(private dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.initTimer();
+  }
+
+  initTimer() {
+    this.timer = setInterval(() => {
+      this.progress = this.progress + 5;
+      if (this.progress >= 100) {
+        this.onStop();
+      }
+    }, 1000);
+  }
+
+  onStop() {
+    clearInterval(this.timer);
+    this.paused = false;
+    const dialogRef$ = this.dialog.open(StopDialogComponent, {
+      data: { progress: this.progress }
+    });
+    dialogRef$.afterClosed().subscribe(result => {
+      if (result) {
+        this.trainingExit.emit();
+      } else {
+        this.onStart();
+      }
+    });
+  }
+
+  onStart() {
+    this.initTimer();
+    this.paused = false;
   }
 
 }
