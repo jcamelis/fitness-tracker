0833ec0f12e4a03914280479c9e0fe3ee80d3b64
Javier Camelis <jcamelis@gmail.com>
2020-09-21 00:06:37 -0300

    Assignment - loading exercises

--- src/app/training/past-trainings/past-trainings.component.ts
+++ src/app/training/past-trainings/past-trainings.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/shared/ui.service';
 import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 
@@ -21,17 +22,28 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy 
   ];
   dataSource = new MatTableDataSource<Exercise>();
 
-  suscription: Subscription;
+  subscriptions: Subscription[] = [];
 
-  constructor(private trainingService: TrainingService) { }
+  loading = false;
+
+  constructor(
+    private trainingService: TrainingService,
+    private uiService: UiService
+  ) { }
 
   ngOnInit(): void {
-    this.suscription = this.trainingService.finishedExercisesChenged
+    this.subscriptions.push(this.trainingService.finishedExercisesChenged
       .subscribe((exercises) => {
         console.log(exercises);
         this.dataSource.data = exercises;
-      });
+      })
+    );
     this.trainingService.fecthCompletedOrCanceled();
+    this.subscriptions.push(
+      this.uiService.loadingStateChanged.subscribe(
+        loading => this.loading = loading
+      )
+    );
   }
 
   ngAfterViewInit(): void {
@@ -44,6 +56,6 @@ export class PastTrainingsComponent implements OnInit, AfterViewInit, OnDestroy 
   }
 
   ngOnDestroy() {
-    this.suscription.unsubscribe();
+    this.subscriptions.forEach(subs => subs.unsubscribe());
   }
 }
